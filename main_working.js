aa51738acb257192c16455112b80d1e9df23b7bb
Cibi <cibivishnukomberi@gmail.com>
2024-05-28 22:38:12 +0530

    working img prompt

--- main_working.js
+++ main_working.js
@@ -3,10 +3,28 @@ var j = document.getElementById("api_key");
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 const API_KEY = j.innerText;
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+
+
+const toDataURL = (url) =>
+  fetch(url)
+    .then((response) => response.blob())
+    .then(
+      (blob) =>
+        new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => resolve(reader.result);
+          reader.onerror = reject;
+          reader.readAsDataURL(blob);
+        })
+    );
+
+
 
 var elements = document.getElementsByClassName("qtext");
 var elements1 = document.getElementsByClassName("answer");
+
 const butt = document.createElement("button");
 butt.style.fontSize = "14px";
 butt.value = "Next page";
@@ -18,73 +36,43 @@ if (elements1.length) {
   elements1[elements1.length - 1].appendChild(butt);
 }
 
+for (let i = 0; i < elements.length; i++) {
 
+    const allImages = elements[i].getElementsByTagName("img");
+    console.log("allImages");
+    console.log(allImages);
 
-
-// for (let i = 0; i < elements.length; i++) {
-  
-// }
-
-
-var allImages = document.querySelector("img");
-  let imageParts = [];
-  const toDataURL = url => fetch(url)
-  .then(response => response.blob())
-  .then(blob => new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onloadend = () => resolve(reader.result)
-    reader.onerror = reject
-    reader.readAsDataURL(blob)
-  }))
-  async function fileToGenerativePart() {
-    
-    toDataURL(allImages.src)
-     .then(dataUrl => {
+    let imageParts = [];
+    async function fileToGenerativePart1() {
+    toDataURL(allImages[0].src)
+        .then((dataUrl) => {
         let data = dataUrl;
-        let base64EncodedDataPromise = data.split(',')[1];
-        let fileType = data.split(',')[0].split(':')[1].split(';')[0];
-        let a= {
-            inlineData: { data:base64EncodedDataPromise, mimeType: fileType },
-          };
+        let base64EncodedDataPromise = data.split(",")[1];
+        let fileType = data.split(",")[0].split(":")[1].split(";")[0];
+        let a = {
+            inlineData: { data: base64EncodedDataPromise, mimeType: fileType },
+        };
         imageParts.push(a);
-     })
-}
-  fileToGenerativePart()
+        })
+        .then(() => {
+        console.log("HI");
+        run();
+        });
+    }
 
-  var textContent = elements[0].innerText;
-  console.log(textContent);
-
-  var textContent1 = elements1[0].innerText;
-  console.log(textContent1);
+  fileToGenerativePart1();
 
   async function run() {
+    const prompt = "What's this image about";
 
-    const prompt = "what is this image";
-    console.log(prompt);
-    let result;
-    // if (allImages.length === 0) {
-    //   result = await model.generateContent(prompt);
-    // } else {
-      console.log("imageParts")
-      console.log(imageParts)
-      result = await model.generateContent([prompt, ...imageParts]);
-    // }
+    console.log("imageParts");
+    console.log(imageParts);
+    const result = await model.generateContent([prompt, ...imageParts]);
     const response = await result.response;
-    
     const text = response.text();
+    console.log(text);
+  }
 
-            const textBox = document.createElement("P");
-            textBox.textContent = text;
-            textBox.style.padding = '5px';
-            textBox.style.color = 'red';
-            textBox.style.fontWeight = 'bold';
-            
-            textBox.style.fontFamily = 'sans-serif';
-            elements[i].appendChild(textBox);
-            
-      }
-
-      run();
-
-
-
+  let a = document.getElementById("asd");
+  a.onclick = run;
+}
